131f14c7a2b81fd9e5262a4a7dcbe8effe526734
Kanitkorn S <k.sujautra@gmail.com>
2016-03-12 17:43:01 +0700

    Clean up myapp

--- myapp/app.js
+++ myapp/app.js
@@ -5,9 +5,6 @@ const favicon = require('serve-favicon')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 
-const routes = require('./routes/index')
-const users = require('./routes/users')
-
 const app = express()
 
 // view engine setup
@@ -62,39 +59,5 @@ app.get(/dog/, (req, res) => {
 app.get('/admin', (req, res) => {
   res.status(200).send('You are admin')
 })
-
-// app.use('/', routes)
-// app.use('/users', users)
-//
-// // catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//   const err = new Error('Not Found')
-//   err.status = 404
-//   next(err)
-// })
-//
-// // error handlers
-//
-// // development error handler
-// // will print stacktrace
-// if (app.get('env') === 'development') {
-//   app.use(function (err, req, res, next) {
-//     res.status(err.status || 500)
-//     res.render('error', {
-//       message: err.message,
-//       error: err
-//     })
-//   })
-// }
-//
-// // production error handler
-// // no stacktraces leaked to user
-// app.use(function (err, req, res, next) {
-//   res.status(err.status || 500)
-//   res.render('error', {
-//     message: err.message,
-//     error: {}
-//   })
-// })
 
 module.exports = app
