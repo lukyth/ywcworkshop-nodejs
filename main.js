e041b7dd3bbe81f3b7d67749e8c6c82d59cca65a
Kanitkorn S <k.sujautra@gmail.com>
2016-03-12 13:57:10 +0700

    Add once event

--- main.js
+++ main.js
@@ -9,7 +9,16 @@ username: ${user.username}
 email: ${user.email}`)
 })
 
+ee.once('hello_once', () => {
+  console.log('This should appear once')
+})
+
 ee.emit('hello', {
   username: "lukyth",
   email: "k.sujautra@gmail.com"
 })
+
+ee.emit('hello_once')
+ee.emit('hello_once')
+ee.emit('hello_once')
+ee.emit('hello_once')
